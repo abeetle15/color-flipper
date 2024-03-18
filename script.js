a1de9818cc1149c83f882619caae05f6503bc708
A Beetle <abeetle15@gmail.com>
2024-03-18 07:54:49 -0500

    set up HTML and CSS.
    add the constants to js.

---
+++ script.js
@@ -0,0 +1,8 @@
+const colors = ['red', 'green', 'rgba(133, 122, 200)', '#f15025'];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function() {
+  
+
+})
